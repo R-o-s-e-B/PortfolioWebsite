cbf8931c8c6a2add3a654c79bd89f040d1db9f27
R-o-s-e-B <134575747+R-o-s-e-B@users.noreply.github.com>
2024-03-29 13:45:59 +0530

    xs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         primary: "#DECEC5",
