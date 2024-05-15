3b6c1f6c537cbf6e2dc0d4f8e97217512f14a1fc
R-o-s-e-B <134575747+R-o-s-e-B@users.noreply.github.com>
2024-05-15 16:31:35 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-
+import ScrollToTop from "./components/scrollToTop.jsx";
 import Landing from "./components/Landing";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
@@ -13,6 +13,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/Work" element={<Work />} />
